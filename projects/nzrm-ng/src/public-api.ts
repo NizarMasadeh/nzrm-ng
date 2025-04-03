ec210e43eae07cb68e9ccda662863368d02eac15
Nizar Masadeh <nizarmasadeh2001@gmail.com>
2025-04-04 02:54:51 +0300

    v0.1.7

--- projects/nzrm-ng/src/public-api.ts
+++ projects/nzrm-ng/src/public-api.ts
@@ -1,10 +1,24 @@
 /*
  * Public API Surface of nzrm-ng
  */
+
+/****TOOLTIP****/
 export * from './lib/components/tooltip/tooltip.directive';
+
+/*****NOTIFICATION****/
 export * from './lib/components/notification/notification.component';
 export * from './lib/components/notification/notification.service';
+
+/*****THEME TOGGLE****/
 export * from './lib/components/theme-toggle/theme-toggle.component';
-export * from './lib/services/theme.service';
 export * from './lib/components/theme-toggle/theme-toggle.module';
+export * from './lib/services/theme.service';
+
+/****CONFIRMATION DIALOG****/
+export * from './lib/components/confirmation-dialog/confirmation-dialog.component';
+export * from './lib/components/confirmation-dialog/confirmation-dialog.service';
+export * from './lib/components/confirmation-dialog/confirmation-dialog.directive';
+export * from './lib/components/confirmation-dialog/confirmation-dialog.model';
+
+/****STYLES****/
 export * from './lib/styles/styles.module';
