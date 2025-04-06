904b19f5be0d8448e7d9a2308126dc345cc50529
Nizar Masadeh <nizarmasadeh2001@gmail.com>
2025-04-06 19:54:27 +0300

    Tooltip enhanced (sticky/hide/changePos)

--- projects/nzrm-ng/src/lib/components/tooltip/tooltip.directive.ts
+++ projects/nzrm-ng/src/lib/components/tooltip/tooltip.directive.ts
@@ -1,11 +1,4 @@
-import {
-    Directive,
-    ElementRef,
-    HostListener,
-    Input,
-    OnInit,
-    Renderer2,
-} from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
     selector: '[nTooltip]',
@@ -14,8 +7,10 @@ import {
 export class TooltipDirective implements OnInit {
     @Input() tooltipText = '';
     @Input() showOnlyIf = true;
+    @Input() sticky = false;
 
     private tooltipElement: HTMLElement | null = null;
+    private isVisible = false;
 
     constructor(
         private el: ElementRef,
@@ -23,28 +18,87 @@ export class TooltipDirective implements OnInit {
     ) { }
 
     ngOnInit() {
-        this.tooltipElement = this.renderer.createElement("div")
-        this.renderer.addClass(this.tooltipElement, "custom-tooltip")
+        this.tooltipElement = this.renderer.createElement("div");
+        this.renderer.addClass(this.tooltipElement, "custom-tooltip");
 
-        const textNode = this.renderer.createText(this.tooltipText)
-        this.renderer.appendChild(this.tooltipElement, textNode)
+        const textNode = this.renderer.createText(this.tooltipText);
+        this.renderer.appendChild(this.tooltipElement, textNode);
 
-        const rect = this.el.nativeElement.getBoundingClientRect()
+        const rect = this.el.nativeElement.getBoundingClientRect();
         this.renderer.setStyle(this.tooltipElement, "top", `${rect.top + window.scrollY - 10}px`);
         this.renderer.setStyle(this.tooltipElement, "left", `${rect.left + window.scrollX + rect.width / 2}px`);
         this.renderer.setStyle(this.tooltipElement, "transform", "translate(-50%, -100%)");
         this.renderer.setStyle(this.tooltipElement, "display", "none");
 
-        this.renderer.appendChild(document.body, this.tooltipElement)
+        this.renderer.appendChild(document.body, this.tooltipElement);
+
+        if (this.sticky && this.showOnlyIf) {
+            this.showTooltip();
+        }
     }
 
     @HostListener('mouseenter')
     @HostListener('touchstart')
     onMouseEnter(): void {
         if (!this.showOnlyIf) return;
+        this.showTooltip();
+    }
+
+    @HostListener('mouseleave')
+    @HostListener('touchend')
+    onMouseLeave(): void {
+        if (this.tooltipElement && !this.sticky) {
+            this.hideTooltip();
+        }
+    }
+
+    @HostListener('click')
+    onClick(): void {
+        if (this.sticky && this.showOnlyIf) {
+            if (this.isVisible) {
+                this.hideTooltip();
+            } else {
+                this.showTooltip();
+            }
+        }
+    }
+
+    @HostListener('window:resize')
+    onResize(): void {
+        if (
+            this.tooltipElement &&
+            this.showOnlyIf &&
+            this.isVisible
+        ) {
+            this.updateTooltipPosition();
+        }
+    }
+
+    private showTooltip(): void {
+        if (!this.tooltipElement) return;
+
+        this.updateTooltipPosition();
+        this.renderer.setStyle(this.tooltipElement, 'opacity', '1');
+        this.renderer.setStyle(this.tooltipElement, 'display', 'block');
+        this.isVisible = true;
+    }
+
+    private hideTooltip(): void {
+        if (!this.tooltipElement) return;
+
+        this.renderer.setStyle(this.tooltipElement, 'opacity', '0');
+        setTimeout(() => {
+            if (this.tooltipElement && !this.isVisible) {
+                this.renderer.setStyle(this.tooltipElement, 'display', 'none');
+            }
+        }, 300);
+        this.isVisible = false;
+    }
+
+    private updateTooltipPosition(): void {
+        if (!this.tooltipElement) return;
 
         const rect = this.el.nativeElement.getBoundingClientRect();
-
         this.renderer.setStyle(
             this.tooltipElement,
             'top',
@@ -55,32 +109,6 @@ export class TooltipDirective implements OnInit {
             'left',
             `${rect.left + window.scrollX + rect.width / 2}px`
         );
-        this.renderer.setStyle(
-            this.tooltipElement,
-            'transform',
-            'translate(-50%, -100%)'
-        );
-        this.renderer.setStyle(this.tooltipElement, 'opacity', '1');
-        this.renderer.setStyle(this.tooltipElement, 'display', 'block');
-    }
-
-    @HostListener('mouseleave')
-    @HostListener('touchend')
-    onMouseLeave(): void {
-        if (this.tooltipElement) {
-            this.renderer.setStyle(this.tooltipElement, 'opacity', '0');
-        }
-    }
-
-    @HostListener('window:resize')
-    onResize(): void {
-        if (
-            this.tooltipElement &&
-            this.showOnlyIf &&
-            window.getComputedStyle(this.tooltipElement).opacity !== '0'
-        ) {
-            this.onMouseEnter();
-        }
     }
 
     ngOnDestroy(): void {
